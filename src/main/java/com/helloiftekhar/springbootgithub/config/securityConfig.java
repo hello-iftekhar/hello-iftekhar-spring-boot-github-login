dbebd3e257971a71145530193447a2e4f7f01679
Iftekhar Hossain <helloiftekhar18@gmail.com>
2024-01-25 13:49:38 +0600

    initial commit

---
+++ src/main/java/com/helloiftekhar/springbootgithub/config/securityConfig.java
@@ -0,0 +1,23 @@
+package com.helloiftekhar.springbootgithub.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(req->req
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
